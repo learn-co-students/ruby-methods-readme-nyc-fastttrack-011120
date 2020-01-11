06c0129354e0fc44a9542f2d050cfe42eaf79192
concurrent-server-0630 <andycruz008@gmail.com>
2020-01-11 16:56:05 +0000

    Done.

--- lib/example.rb
+++ lib/example.rb
@@ -1,13 +1,14 @@
-# Use this space to code along with the readme
+def hello_world_ten_times
+  phrase = "Hello World!"
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase 
+end
 
-phrase = "Hello World!"
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
+hello_world_ten_times
