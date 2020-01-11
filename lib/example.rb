0f61d2fe8bececbd081ad4a0d5802c3e976023ed
Jonathan T Dean <jonathantomdean@gmail.com>
2020-01-11 16:28:32 +0000

    Done.

--- lib/example.rb
+++ lib/example.rb
@@ -1,13 +1,13 @@
 # Use this space to code along with the readme
 
-phrase = "Hello World!"
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
+def say_hello_world_ten_times()
+  i = 0
+  phrase = "Hello World!"
+    while i < 10
+      puts phrase
+      
+      i += 1
+    end
+end
+
+say_hello_world_ten_times
